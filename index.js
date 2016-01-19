a50f8ff09816232c818e1563320c2757afefd8b1
Michael Milton <ttmigueltt@gmail.com>
2016-01-19 06:46:59 +0100

    Basic tree

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
 
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
 var pgp = require("pg-promise")(/*options*/);
 var db = pgp(process.env.DATABASE_URL);
 
 // respond with "hello world" when a GET request is made to the homepage
 
+app.use(morgan('combined'));
 app.use('/static', express.static('static'));
 app.get('/', (req, res) => {
     res.sendFile('static/index.html', {root: __dirname});
@@ -22,12 +24,22 @@ app.get('/api/subreddits', (req, res) => {
 });
 
 app.get('/api/markov', (req, res) => {
-    let word = req.params.w;
-    let subreddit = req.params.s;
-
-    db.many("SELECT * FROM nodes WHERE ")
+    db.one(`
+        SELECT
+            top_ten
+        FROM
+            node
+            JOIN subreddit ON node.subreddit_id = subreddit.id
+	        JOIN node_top_ten ON node_top_ten.id = node.id
+        WHERE
+            subreddit.name = $1
+            AND node.state[1] = $2
+            AND node.state[2] = $3
+            --AND key ~ '[[:alnum:]_]';
+    `, [req.query.sub, req.query.s1, req.query.s2]
+        )
         .then(function (data) {
-            res.send(data.map(row => row.name));
+            res.send(data);
         })
         .catch(function (error) {
             console.log("ERROR:", error);
--- src/index.js
+++ src/index.js
@@ -1,100 +1,299 @@
 import d3 from "d3";
 import Vue from 'vue'
+import clone from 'clone'
+import copy from 'shallow-copy';
 
+const outerDimensions = {
+    width: 1000,
+    height: 1000
+};
+const margins = {
+    x: 100,
+    y: 100
+};
 const dimensions = {
-    width: 960,
-    height: 500
+    width: outerDimensions.width - (2 * margins.x),
+    height: outerDimensions.height - (2 * margins.y)
 };
 
-const color = d3.scale.category20();
+const LINK_WIDTH = 5;
+const DEFAULT_NODES = {
+    name: "___BEGIN__",
+    children: [{
+        name: "___BEGIN__",
+        state: 'open',
+        weight: 1
+    }],
+    x0: dimensions.height / 2,
+    y0: 0,
+    state: 'open'
+};
+const duration = 750;
+const DEFAULT_STATE = [0, 1]; //By default the current state is the first two nodes: ___BEGIN__
 
-const force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([dimensions.width, dimensions.height]);
+const diagonal = d3.svg.diagonal()
+    .projection(function (d) {
+        return [d.y, d.x];
+    });
 
-const svg = d3.select("body").append("svg")
-    .attr("width", dimensions.width)
-    .attr("height", dimensions.height);
+let tree = d3.layout.tree()
+    .size([dimensions.width - (2 * margins.x), dimensions.height - (2 * margins.y)])
+    .separation((a, b) => {
+        return 0.5;// return a.parent == b.parent ? 1 : 1;
+    });
 
-new Vue({
+const outerSvg = d3.select("#chart").append("svg")
+    .attr("width", outerDimensions.width)
+    .attr("height", outerDimensions.height);
+
+const svg = outerSvg
+    .append("g")
+    .attr("transform", `translate(${margins.x}, ${margins.y})`);
+
+outerSvg.append("defs").html(`
+  <pattern id="loader" patternUnits="userSpaceOnUse" width="100" height="100">
+    <image xlink:href="static/loader.gif" x="0" y="0" width="100" height="100" />
+  </pattern>`);
+
+const vm = new Vue({
     el: '#main',
     data: {
         currentSubreddit: 'AskReddit',
         subreddits: [],
-        tree: [],
-        currentWord: "___BEGIN__"
+        root: copy(DEFAULT_NODES),
+        //links: copy(DEFAULT_LINKS),
+        state: [],
+        nextNodeId: 0
     },
-    methods: {},
+    computed: {
+        secondRoot(){
+            return this.root.children[0];
+        }
+    },
+    methods: {
+        clickNode(d) {
+            if (!d.children && !d._children) {
+                this.fetchData(d);
+            }
+            else {
+                if (d.children) {
+                    d._children = d.children;
+                    d.children = null;
+                } else {
+                    d.children = d._children;
+                    d._children = null;
+                }
+                this.render(d);
+            }
+        },
+        render(source) {
+
+            var nodes = tree.nodes(this.root).reverse();
+            var links = tree.links(nodes);
+
+            // Normalize for fixed-depth.
+            //nodes.forEach(function (d) {
+            //    d.y = d.depth * 90;
+            //});
+
+            // Update the nodes…
+            var node = svg.selectAll("g.node")
+                .data(nodes, d => {
+                    return d.id || (d.id = this.nextNodeId++);
+                });
+
+            node.style("fill", d => {
+                switch (d.state) {
+                    case "open":
+                        return "white";
+                    case "closed":
+                        return "black";
+                    case "loading":
+                        return "url(#loader)";
+
+                }
+            });
+
+            // Enter any new nodes at the parent's previous position.
+            var nodeEnter = node.enter().append("g")
+                .attr("class", "node")
+                .attr("transform", d => {
+                    return "translate(" + source.y0 + "," + source.x0 + ")";
+                })
+                .on("click", this.clickNode);
+
+            nodeEnter.append("circle")
+                .attr("r", 1e-6)
+            //.style("fill", function (d) {
+            //    return d._children ? "white" : "#fff";
+            //});
+
+            nodeEnter.append("text")
+                .attr("x", function (d) {
+                    return d.children || d._children ? -10 : 10;
+                })
+                .attr("dy", ".35em")
+                .attr("text-anchor", function (d) {
+                    return d.children || d._children ? "end" : "start";
+                })
+                .text(function (d) {
+                    let weight = d.weight ? ` (${d.weight})` : "";
+                    return `${d.name}${weight}`;
+                })
+                .style('fill', 'white')
+                .style("fill-opacity", 1e-6);
+
+            // Transition nodes to their new position.
+            var nodeUpdate = node.transition()
+                .duration(duration)
+                .attr("transform", function (d) {
+                    return "translate(" + d.y + "," + d.x + ")";
+                });
+
+            nodeUpdate.select("circle")
+                .attr("r", 4.5)
+            //.style("fill", function (d) {
+            //    return d._children ? "lightsteelblue" : "#fff";
+            //});
+
+            nodeUpdate.select("text")
+                .style("fill-opacity", 1);
+
+            // Transition exiting nodes to the parent's new position.
+            var nodeExit = node.exit().transition()
+                .duration(duration)
+                .attr("transform", function (d) {
+                    return "translate(" + source.y + "," + source.x + ")";
+                })
+                .remove();
+
+            nodeExit.select("circle")
+                .attr("r", 1e-6);
+
+            nodeExit.select("text")
+                .style("fill-opacity", 1e-6);
+
+            // Update the links…
+            var link = svg.selectAll(".link")
+                .data(links, function (d) {
+                    return d.target.id;
+                });
+
+            // Enter any new links at the parent's previous position.
+            var linkEnter = link
+                .enter()
+                .append('g')
+                .attr("class", "link");
+
+            //link
+            //    .selectAll('path')
+            //    .attr("d", function (d) {
+            //        var o = {x: source.x, y: source.y};
+            //        return diagonal({source: o, target: o});
+            //    })
+            //    .style('stroke-width', d => (d.target.weight / d.source.totalWeight * 20) + 'px');
+
+            var linkLine = linkEnter
+                .append("path")
+                .attr("d", function (d) {
+                    var o = {x: source.x, y: source.y};
+                    return diagonal({source: o, target: o});
+                })
+                .attr('stroke', 'white')
+                .style('fill', 'none')
+                .style('stroke-width', d => (d.target.weight / d.source.totalWeight * 20) + 'px');
+
+            //linkEnter
+            //    .append("text")
+            //    .attr("transform", function (d) {
+            //        let x = (d.source.x + d.target.x) /2;
+            //        let y = (d.source.y + d.target.y) /2;
+            //        return `translate(${y}, ${x})`;
+            //    })
+            //    .style('stroke', 'white')
+            //    .text(d => d.target.weight);
+
+            // Transition links to their new position.
+            linkLine.transition()
+                .duration(duration)
+                .attr("d", diagonal);
+
+            link.exit().remove();
+
+            // Transition exiting nodes to the parent's new position.
+            //link.exit().selectAll('line').transition()
+            //    .duration(duration)
+            //    .attr("d", function (d) {
+            //        var o = {x: source.x0, y: source.y0};
+            //        return diagonal({source: o, target: o});
+            //    })
+            //    .remove();
+
+            // Stash the old positions for transition.
+            nodes.forEach(function (d) {
+                d.x0 = d.x;
+                d.y0 = d.y;
+            });
+        },
+
+        fetchData(node){
+            node.state = 'loading';
+            this.render(node);
+            const sub = this.currentSubreddit;
+            const s1 = node.parent.name;
+            const s2 = node.name;
+            d3.json(`/api/markov?sub=${sub}&s1=${s1}&s2=${s2}`, (error, json) => {
+
+                //Process the data
+                let result = json.top_ten;
+                result.forEach(node => {
+                    node.state = 'closed';
+                });
+
+                //Update the parent node
+                node.totalWeight = result.reduce((prev, curr) => prev + curr.weight, 0);
+                node.state = 'open';
+                node.children = result;
+
+                //Rerender
+                this.render(this.root);
+            });
+        },
+        loadInitial(){
+            this.fetchData(this.secondRoot);
+        }
+    },
     ready(){
+
+        this.state.push(this.root, this.root.children[0]);
+
         //On load, get the list of subreddits
         d3.json("/api/subreddits", (error, json) => {
             this.subreddits = json;
         });
+
+        this.render(this.root);
     },
     watch: {
-        //Whenever the current subreddit changes, download new data
-        currentSubreddit(sub){
-            d3.json(`/api/markov?s=${sub}?w=${this.currentWord}`, (error, json) => {
-                this.tree = json;
-            });
+        subreddits(){
+            //Whenever the subreddit list is updated, fetch nodes
+            this.loadInitial();
         },
-        tree(val){
+        //Whenever the current subreddit changes, download new data
+        currentSubreddit(){
+            this.secondRoot.children = [];
+            this.loadInitial();
+            this.render(this.root);
+        },
+        links(val){
             //Whenever the tree updates, rerender it with d3
-            render(val);
-        }
+            this.render(val);
+        },
+        //state(){
+        //    //Whenever the word updates, fetch the new nodes
+        //    this.loadInitial();
+        //}
     }
 });
 
-
-function render(tree) {
-    force
-        .nodes(tree.nodes)
-        .links(tree.links)
-        .start();
-
-    var link = svg.selectAll(".link")
-        .data(graph.links)
-        .enter().append("line")
-        .attr("class", "link")
-        .style("stroke-width", function (d) {
-            return Math.sqrt(d.value);
-        });
-
-    var node = svg.selectAll(".node")
-        .data(graph.nodes)
-        .enter().append("circle")
-        .attr("class", "node")
-        .attr("r", 5)
-        .style("fill", function (d) {
-            return color(d.group);
-        })
-        .call(force.drag);
-
-    node.append("title")
-        .text(function (d) {
-            return d.name;
-        });
-
-    force.on("tick", function () {
-        link.attr("x1", function (d) {
-                return d.source.x;
-            })
-            .attr("y1", function (d) {
-                return d.source.y;
-            })
-            .attr("x2", function (d) {
-                return d.target.x;
-            })
-            .attr("y2", function (d) {
-                return d.target.y;
-            });
-
-        node.attr("cx", function (d) {
-                return d.x;
-            })
-            .attr("cy", function (d) {
-                return d.y;
-            });
-    });
-}
+window.vm = vm;
